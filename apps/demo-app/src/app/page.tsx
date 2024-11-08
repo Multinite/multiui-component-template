182a47b9a1840dee23dd43e66ad855d977e77eec
Multinite <founders.multinite@gmail.com>
2024-11-08 13:49:53 +1000

    fix: Updated component & imported to next app

--- apps/demo-app/src/app/page.tsx
+++ apps/demo-app/src/app/page.tsx
@@ -1,4 +1,17 @@
-import Demo from "../../../../packages/react/src/demo";
+import Component from "my-component";
+
 export default function Home() {
-  return <Demo />;
+  return (
+    <div>
+      <h1>No Children</h1>
+      {/* @ts-ignore */}
+      <Component />
+
+      <hr />
+      <h1>With Children</h1>
+      <Component>
+        <p>Hello World</p>
+      </Component>
+    </div>
+  );
 }
